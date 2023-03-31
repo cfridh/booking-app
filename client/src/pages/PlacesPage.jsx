1cc1b709aaa90ef711a59b8c182fcb40537d6e42
Christopher Fridh <christopherfridh@gmail.com>
2023-03-31 13:32:44 +0200

    bug fix

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -110,11 +110,11 @@ async function addPhotoByLink (ev) {
             <div className="mt-2 grid fap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6  ">
                 
                     {addedphotos.length > 0 && addedphotos.map(link => (
-                        <div>
-                        <img className="rounded-2xl" src={'http://localhost:3000/uploads/' + link} alt="" />
+                        <div className='h-32 flex' >
+                        <img className="rounded-2xl w-full object-cover" src={'http://localhost:3000/uploads/' + link} alt="" />
                         </div>
                     ))}
-                <label className='flex items-center cursor-pointer justify-center gap-2 border bg-ransparent rounded-2xl p-2 text-2xl text-gray-600' >
+                <label className='h-32 flex items-center cursor-pointer justify-center gap-2 border bg-ransparent rounded-2xl p-2 text-2xl text-gray-600' >
                 <input type="file" multiple className='hidden'onChange={uploadPhoto} />
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
