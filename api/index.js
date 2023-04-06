4c1ee96e053ef933fe7ec759109bb4f8767f7dd3
Christopher Fridh <christopherfridh@gmail.com>
2023-04-06 12:10:19 +0200

    PhotoUp

--- api/index.js
+++ api/index.js
@@ -102,7 +102,7 @@ for (let i = 0; i < req.files.length; i++) {
   const {path, originalname} = req.files[i];
   const parts = originalname.split('.')
   const ext = parts[parts.length - 1]
-  const newPath = path + '.' + ext
+  const newPath = path + '.' + ext 
   fs.renameSync(path, newPath)
   uploadedFiles.push(newPath.replace('uploads\\', ''))
 }
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -4,14 +4,14 @@ import axios from 'axios'
 import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import Perks from '../Perks'
+import PhotosUploader from '../PhotosUploader'
 
 
 function PlacesPage() {
 const {action} = useParams()
 const [title,setTitle] = useState('')
 const [address,setAddress] = useState('')
-const [addedphotos,setAddedPhotos] = useState([])
-const [photoLink,setPhotoLink] = useState('')
+const [addedPhotos,setAddedPhotos] = useState([])
 
 const [description,setDescription] = useState('')
 const [perks,setPerks] = useState([])
@@ -42,31 +42,7 @@ function preInput(header, description) {
 }
 
 
-async function addPhotoByLink (ev) {
-    ev.preventDefault()
-    const {data:filename} = await axios.post('/upload-by-link', {link: photoLink})
-    setAddedPhotos(prev => {
-        return [...prev, filename]
-    })
-    setPhotoLink('')
- }
 
-    function uploadPhoto(ev) {
-    const files = ev.target.files;
-    const data = new FormData();
-    for (let i = 0; i < files.length; i++) {
-        data.append('photos', files[i])   
-    }
-    axios.post('/upload', data, {
-        headers: {'Content-Type': 'multipart/form-data'}
-    }).then(response => {
-        const {data:filenames} = response
-        setAddedPhotos(prev => {
-            return [...prev, ...filenames]
-        })
-
-    })
-}
 
 
 
@@ -97,32 +73,7 @@ async function addPhotoByLink (ev) {
             <input type="text" value={address} onChange={ev => setAddress(ev.target.value)} placeholder='address' />
             {preInput('Photos','Add photos of your place')}
             
-            <div className="flex  gap-2">
-                <div className="w-3/4">
-                   
-                
-                <input type="text" value={photoLink} onChange={ev => setPhotoLink(ev.target.value)} placeholder='photo link' />
-                </div>
-                <div className="w-1/4">
-                <button onClick={addPhotoByLink} className=' primary ' >Upplod photo</button>
-                </div>
-            </div>
-            <div className="mt-2 grid fap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6  ">
-                
-                    {addedphotos.length > 0 && addedphotos.map(link => (
-                        <div className='h-32 flex' >
-                        <img className="rounded-2xl w-full object-cover" src={'http://localhost:3000/uploads/' + link} alt="" />
-                        </div>
-                    ))}
-                <label className='h-32 flex items-center cursor-pointer justify-center gap-2 border bg-ransparent rounded-2xl p-2 text-2xl text-gray-600' >
-                <input type="file" multiple className='hidden'onChange={uploadPhoto} />
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                            </svg>
-                Upload
-                </label>
-
-            </div>
+            <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
 
            {preInput('Description','Describe your place')}
             
